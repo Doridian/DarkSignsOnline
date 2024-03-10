55b20eed5f07a24ea54c7799a8487afa2709536a
Doridian <git@doridian.net>
2024-03-10 14:40:35 -0700

    Add termsofuse.php and show git revision hash

--- server/_bottom.php
+++ server/_bottom.php
@@ -1,32 +1,29 @@
-                  <br /><!-- cut off -->
-                  <br />
-                  <center>
-                    <br />
-                    <span class="style3"
-                      ><span class="style5">. . . </span><br />
-                      <strong>Dark Signs Online</strong><br />Copyright &copy;
-                      2008 Dark Signs Online<br /><a
-                        href="https://web.archive.org/web/20101231205551/http://www.darksignsonline.com/index.php"
-                        >Home</a
-                      >
-                      |
-                      <a
-                        href="https://web.archive.org/web/20101231205551/http://www.darksignsonline.com/chatlog.php"
-                        >Live Chat Log</a
-                      >
-                      |
-                      <a
-                        href="https://web.archive.org/web/20101231205551/http://www.darksignsonline.com/termsofuse.php"
-                        >Terms of use</a
-                      ><br /><br /><br /><br /><br />
-                    </span>
-                  </center>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </body>
-    </html>
-  </head>
+<br /><!-- cut off -->
+<br />
+<center>
+  <br />
+  <span class="style3"><span class="style5">. . . </span><br />
+    <strong>Dark Signs Online version
+      <?php echo file_get_contents('api/gitrev.txt'); ?>
+    </strong><br />
+    Copyright &copy; 2008-
+    <?php date('Y'); ?> Dark Signs Online<br />
+    <a href="/index.php">Home</a>
+    |
+    <a href="https://web.archive.org/web/20101231205551/http://www.darksignsonline.com/chatlog.php">Live Chat Log
+      (archive.org)</a>
+    |
+    <a href="/termsofuse.php">Terms of use</a><br /><br /><br /><br /><br />
+  </span>
+</center>
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+</body>
+
+</html>
+</head>
+
 </html>
---
+++ termsofuse.php
@@ -0,0 +1,42 @@
+<?php require('_top.php'); ?>
+<span class="style5"><br />
+    <p><br />
+        <font face="Georgia, Times New Roman, Times, serif" size="+3">Conditions </font><br />
+        <br />
+    <table width="80%" border="0">
+        <tr>
+            <td>
+                <div align="left">
+                    <font face="Georgia" size="3">
+                        <strong>By participating in Dark Signs Online, you agree to the following terms: </strong><br />
+                        <br />
+                        (1) Adminitrators and moderators of Dark Signs Online are generally lenient, however you agree
+                        to accept any decision made by an authority individual regarding your Dark Signs Online account,
+                        either inside or outside of the Dark Signs Online environment. <br />
+                        <br />
+                        (2) You may not register and use more than 3 accout usernames on the Dark Signs Network.<br />
+                        <br />
+                        (3) You may not steal, hijack, or log in to accounts which are registered to players other than
+                        yourself. <br />
+                        <br />
+                        (4) You may not upload illegal content to Dark Signs Online. <br />
+                        <br />
+                        (5) All content created by you and uploaded to Dark Signs Online becomes part of the Dark Signs
+                        Network. Dark Signs Online reserves the right to continue to use any such content indefinately
+                        in any form, should you decide to no longer participate.<br />
+                        <br />
+                        (6) Your account may be suspended or terminated without notification if any of these conditions
+                        are not met. <br />
+                        <br />
+                        This page may be updated in the future.<br />
+                    </font>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <br />
+    <br />
+    <br />
+    </p>
+</span>
+<?php require('_bottom.php'); ?>
