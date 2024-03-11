306183b7f59381aabd0bda623cf1874a04ede710
Doridian <git@doridian.net>
2024-03-11 12:16:30 -0700

    echo some things

--- client/mkdirs.bat
+++ client/mkdirs.bat
@@ -5,3 +5,8 @@ mkdir user\downloads
 mkdir user\system
 mkdir user\system\commands
 mkdir user\temp
+
+echo "Put .mp3 files here for ingame music" > user\home\mp3\README.txt
+echo "This is where DSO will store downloaded files" > user\downloads\README.txt
+echo "Any .ds script here will be available as a command" > user\system\commands\README.txt
+echo "Temporary files will be stored here" > user\temp\README.txt
