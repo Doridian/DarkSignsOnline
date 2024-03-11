f252a317b3cc23f8a9bd35497f8afc07f40ea2b8
Doridian <git@doridian.net>
2024-03-11 14:02:17 -0700

    GH API key

--- server/api/config.example.php
+++ server/api/config.example.php
@@ -5,5 +5,8 @@ if (!$db) {
     die('9999');
 }
 
+global $github_api_key;
+$github_api_key = ''; // Leave blank for none
+
 //error_reporting(E_ALL & ~E_NOTICE);
 //ini_set('display_errors', 'On');
