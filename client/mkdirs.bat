268e0e3766a07e6ff151ac3efff7613f65b51318
Doridian <git@doridian.net>
2024-03-11 12:13:07 -0700

    backslash?

--- client/mkdirs.bat
+++ client/mkdirs.bat
@@ -1,7 +1,7 @@
 mkdir user
-mkdir user/home
-mkdir user/home/mp3
-mkdir user/downloads
-mkdir user/system
-mkdir user/system/commands
-mkdir user/temp
+mkdir user\home
+mkdir user\home\mp3
+mkdir user\downloads
+mkdir user\system
+mkdir user\system\commands
+mkdir user\temp
