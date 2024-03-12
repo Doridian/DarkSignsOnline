f708d13d173b177e3f00c47154cb1107f09ce498
Doridian <git@doridian.net>
2024-03-11 18:11:13 -0700

    Add releases.json when deploying website

--- server/download.php
+++ server/download.php
@@ -1,4 +1,26 @@
-<?php require('_top.php'); ?>
+<?php
+    require('_top.php');
+
+    function get_release_client($release) {
+        foreach (json_decode($release['assets']) AS $asset) {
+            if ($asset['name'] !== 'client.zip') {
+                continue;
+            }
+            return $asset;
+        }
+        return null;
+    }
+
+    function get_release_date($release) {
+        foreach (json_decode($release['assets']) AS $asset) {
+            if ($asset['name'] !== 'client.zip') {
+                continue;
+            }
+            return $asset['updated_at'];
+        }
+        return 'N/A';
+    }
+?>
 <br /><span class="style5"><br />
     <p><br />
         <font face="Georgia, Times New Roman, Times, serif" size="+3">Download</font><br />
@@ -6,16 +28,24 @@
         <font face="Verdana" size="2">Make sure you also have an <a href="/create_account.php">account</a> ready.</font>
         <br /><br />
     <table width="60%" border="0">
+        <?php
+            foreach(json_decode(file_get_contents('releases.json')) AS $release) {
+                $client = get_release_client($release);
+                if (empty($client)) {
+                    continue;
+                }
+        ?>
         <tr>
             <td width="34%">
-                <font face="Verdana" size="2"><strong>Latest build</strong><br />
-                    Updated Regularily</font>
+                <font face="Verdana" size="2"><strong><?php echo htmlentities($release['name']); ?></strong><br />
+                    Updated <?php echo htmlentities($client['updated_at']); ?></font>
             </td>
             <td width="66%">
                 <div align="right"><a
-                        href="https://github.com/Doridian/DarkSignsOnline/releases/download/latest/client.zip">Download</a><br /></div>
+                        href="<?php echo htmlentities($asset['browser_download_url']); ?>">Download</a><br /></div>
             </td>
         </tr>
+        <?php } ?>
         <tr>
             <td>&nbsp;</td>
             <td>
