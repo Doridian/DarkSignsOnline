0dc00e165ecc959f3f2897e7b1bafe890439a859
Doridian <git@doridian.net>
2024-03-18 13:22:38 -0700

    Add JWT library

---
+++ server/api/domain_token.php
@@ -0,0 +1,39 @@
+<?php
+
+$rewrite_done = true;
+require_once 'function.php';
+
+echo '4100';
+
+$port = (int) $_REQUEST['port'];
+
+if ($port < 1 || $port > 65536) {
+	die ('not found');
+}
+
+$d = strtolower($_REQUEST['d']);
+if (empty($d)) {
+    die ('not found');
+}
+
+require_once('jwt/BeforeValidException.php');
+require_once('jwt/CachedKeySet.php');
+require_once('jwt/ExpiredException.php');
+require_once('jwt/JWK.php');
+require_once('jwt/JWT.php');
+require_once('jwt/JWTExceptionWithPayloadInterface.php');
+require_once('jwt/Key.php');
+require_once('jwt/SignatureInvalidException.php');
+
+use Firebase\JWT\JWT;
+
+$key = 'example_key';
+$payload = [
+    'iss' => 'http://example.org',
+    'aud' => 'http://example.com',
+    'iat' => 1356999524,
+    'nbf' => 1357000000
+];
+
+$jwt = JWT::encode($payload, $key, 'HS256', null);
+echo $jwt;
