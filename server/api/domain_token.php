0043773d08833eafa22ba368a4cd187293fe5e3d
Doridian <git@doridian.net>
2024-03-18 13:43:31 -0700

    Sign with real dates

--- server/api/domain_token.php
+++ server/api/domain_token.php
@@ -3,8 +3,6 @@
 $rewrite_done = true;
 require_once 'function.php';
 
-echo '2000';
-
 $port = (int) $_REQUEST['port'];
 
 if ($port < 1 || $port > 65536) {
@@ -27,12 +25,14 @@ require_once('jwt/JWT.php');
 
 use Firebase\JWT\JWT;
 
+$start = time();
 $payload = [
-    'iss' => 'http://example.org',
-    'aud' => 'http://example.com',
-    'iat' => 1356999524,
-    'nbf' => 1357000000
+    'iss' => 'http://darksignsonline.com',
+    'aud' => "http://$d:$port",
+    'sub' => ''.$user['id'],
+    'name' => $user['username'],
+    'iat' => $start,
+    'exp' => $start + (5 * 60),
 ];
 
-$jwt = JWT::encode($payload, $JWT_PRIVATE_KEY, 'RS256');
-echo $jwt;
+echo JWT::encode($payload, $JWT_PRIVATE_KEY, 'RS256');
