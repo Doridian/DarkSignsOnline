beac3de4fb6c4cc4a18285bcbf35fd006e3a86b5
Doridian <git@doridian.net>
2024-03-19 20:29:32 -0700

    Rework ping.php

--- server/api/ping.php
+++ server/api/ping.php
@@ -1,37 +1,27 @@
 <?php
-	include_once 'function.php';
-	
-	if ($auth == '1001')
-	{
-		$domain = $_POST['domain'];
-		$port = (int)$_POST['port'];
+$rewrite_done = true;
+require_once("function.php");
 
-		$dInfo = getDomainInfo($domain);
-		if ($dInfo[0] > 0)
-		{
-			if($port == 0)
-			{
-				echo 1;
-			}
-			else
-			{
-				$query = $db->query("SELECT domain_id FROM domain_scripts WHERE domain_id=$dInfo[0] AND port=$port");
-				
-				//list($isport) = $db->fetch_row($db->query('SELECT code FROM domain_scripts WHERE domain_id = "'.$dInfo[0].'" AND port = "'.$port.'";'));
-				//if (!empty($isport))
-				
-				if ($db->num_rows($query) == 1)
-				{
-					echo 1;
-				}
-				else
-				{
-					echo 0;
-				}
-			}
-		}
-		else
-		{
-			echo 0;
-		}		
-	}
+$domain = $_POST['domain'];
+$port = (int)$_POST['port'];
+
+$dInfo = getDomainInfo($domain);
+if ($dInfo[0] < 0)
+{
+	die('0');
+}
+if($port === 0)
+{
+	die('1');
+}
+
+$stmt = $db->prepare('SELECT domain_id FROM domain_scripts WHERE domain_id=? AND port=?;');
+$stmt->bind_param('ii', $dInfo[0], $port);
+$stmt->execute();
+$query = $stmt->get_result();
+if($query->num_rows > 0)
+{
+	die('1');
+}
+
+die('0');
