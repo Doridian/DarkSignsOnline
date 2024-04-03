23800b9c5c9de3db1b1ef294f8992ffc08fe7b8e
Doridian <git@doridian.net>
2024-04-02 22:05:31 -0700

    Fix up minor coding mistakes

--- server/api/transfer.php
+++ server/api/transfer.php
@@ -6,7 +6,6 @@ global $db;
 print_returnwith();
 
 $amount = (int)$_REQUEST['amount'];
-$amount2 = number_format($amount);
 
 $to = userToId($_REQUEST['to']);
 $description = $_REQUEST['description'];
@@ -18,7 +17,7 @@ if ($ver > 1) {
 
 $usercash = getCash($user['id']);
 if ($status === 'COMPLETE') {
-	die("Payment of $$amount2 to $to is complete. Your new balance is $$usercash.00");
+	die("Payment of $$amount.00 to $to is complete. Your new balance is $$usercash.00");
 }
 
-die_error("Payment of $$amount2 to $to was DECLINED by the bank with error: $status");
+die_error("Payment of $$amount.00 to $to was DECLINED by the bank with error: $status");
