066b4949342066b127bc3697a1d3d3f47823ad76
Doridian <git@doridian.net>
2024-04-12 12:11:30 -0700

    Oh, we forgot filedata existed

--- server/api/file_database.php
+++ server/api/file_database.php
@@ -71,8 +71,8 @@ if (!empty($shortfilename)){
 
     $timestamp = time();
     $aip = $_SERVER['REMOTE_ADDR'];
-    $stmt = $db->prepare('INSERT INTO file_database (filename, version, title, description, category, createtime, ip, deleted, owner, ver) VALUES (?,?,?,?,?,?,?,0,?,?)');
-    $stmt->bind_param('sssssisii', $shortfilename, $_REQUEST['version'], $_REQUEST['title'], $_REQUEST['description'], $_REQUEST['category'], $timestamp, $aip, $user['id'], $ver);
+    $stmt = $db->prepare('INSERT INTO file_database (filename, filedata, version, title, description, category, createtime, ip, deleted, owner, ver) VALUES (?,?,?,?,?,?,?,?,0,?,?)');
+    $stmt->bind_param('ssssssisii', $shortfilename, $_REQUEST['filedata'], $_REQUEST['version'], $_REQUEST['title'], $_REQUEST['description'], $_REQUEST['category'], $timestamp, $aip, $user['id'], $ver);
     $stmt->execute();
 
     die("Upload complete!");
