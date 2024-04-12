943c79d75d2e19f44fd8f4d5cc68db46b5792aab
Doridian <git@doridian.net>
2024-04-12 12:00:18 -0700

    o

--- server/api/file_database.php
+++ server/api/file_database.php
@@ -84,9 +84,9 @@ $shortfilename = $_REQUEST['shortfilename'];
 if (!empty($shortfilename)){
     $timestamp = time();
     $aip = $_SERVER['REMOTE_ADDR'];
-    $stmt = $db->prepare('INSERT INTO file_database (filename, version, title, description, category, createtime, ip, deleted, owner, ver) VALUES (?,?,?,?,?,?,0,?,?)') or die($db->error);
+    $stmt = $db->prepare('INSERT INTO file_database (filename, version, title, description, category, createtime, ip, deleted, owner, ver) VALUES (?,?,?,?,?,?,?,0,?,?)');
     $stmt->bind_param('sssssisii', $shortfilename, $_REQUEST['version'], $_REQUEST['title'], $_REQUEST['description'], $_REQUEST['category'], $timestamp, $aip, $user['id'], $ver);
-    $stmt->execute() or die($db->error);
+    $stmt->execute();
 
     die("Upload complete!");
 }
