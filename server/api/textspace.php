e93b1af60a4f9dc24b0d9cdb2d4c5acd4213be08
Doridian <git@doridian.net>
2024-04-12 12:16:14 -0700

    Actually, these arent even called that

--- server/api/textspace.php
+++ server/api/textspace.php
@@ -5,11 +5,15 @@ require_once('function.php');
 
 $action = $_REQUEST['action'];
 
-if ($action === 'download')
-{
-	$data = (int)$_REQUEST['data'];
+$download = $_REQUEST['download'];
+if (!empty($download)) {
+	$download_int = (int)$download;
+	if ($download_int < 1) {
+		die_error('4501  ', 404);
+	}
+
 	$stmt = $db->prepare('SELECT `text` FROM textspace WHERE chan = ? ORDER BY id DESC LIMIT 1');
-	$stmt->bind_param('i', $data);
+	$stmt->bind_param('i', $download_int);
 	$stmt->execute();
 	$res = $stmt->get_result();
 	$row = $res->fetch_array();
@@ -21,11 +25,10 @@ if ($action === 'download')
 	die('4501'.$row['text']);
 }
 
-if ($action === 'upload')
-{
-	$chan = (int)$_REQUEST['chan'];
-	if ($chan <= 1)
-	{
+$upload = $_REQUEST['upload'];
+if (!empty($upload)) {
+	$upload_int = (int)$upload;
+	if ($upload_int <= 1) {
 		die_error('4500Invalid channel.', 400);
 	}
 
@@ -33,8 +36,8 @@ if ($action === 'upload')
 	$time = time();
 
 	$stmt = $db->prepare('INSERT INTO textspace (`chan`, `owner`, `lastupdate`, `text`, `deleted`) VALUES (?, ?, ?, ?, 0)');
-	$stmt->bind_param('iiis', $chan, $user['id'], $time, $data);
+	$stmt->bind_param('iiis', $upload_int, $user['id'], $time, $data);
 	$stmt->execute();
 
-	die("4500Updated: $chan!");
+	die("4500Updated: $upload_int!");
 }
