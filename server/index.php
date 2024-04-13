5b1581dfe5a4b58d8674eb7f1b1bdedf2d35436b
Doridian <git@doridian.net>
2024-04-13 14:43:53 -0700

    New article

--- server/index.php
+++ server/index.php
@@ -2,6 +2,12 @@
 <span class="style5"><br />- News - <br /><br />
 
   <strong>April 13th 2024</strong><br /><span class="style9">
+    Added a serverside notification system to the client.<br />
+    Currently, this is used to notify clients of available updates.<br />
+    But it might be used for other important system notifications.<br />
+    Needless to say, if you played the game, please update your versions now :)</span><br /><br />
+
+  <strong>April 13th 2024</strong><br /><span class="style9">
     Fixed the Script Library and DSMail.<br />
     This concludes work on the API and client (for now)<br />
     Just need to update the wiki now...</span><br /><br />
