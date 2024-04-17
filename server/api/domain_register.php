68f04991e2257c5a3c3e8b518b08c78025bf8608
Doridian <git@doridian.net>
2024-04-16 23:27:27 -0700

    fixup

--- server/api/domain_register.php
+++ server/api/domain_register.php
@@ -12,6 +12,7 @@ $price['edu'] = 299;
 $price['mil'] = 1499;
 $price['gov'] = 1499;
 $price['dsn'] = 12999;
+$price['ru'] = 40;
 
 $uid = $user['id'];
 
@@ -67,7 +68,7 @@ if (sizeof($domain) == 4 && validIP($d)) {
 } else if (sizeof($domain) == 2) {
 	// Normal domain register.
 	$ext = $domain[1];
-	if ($ext == 'com' || $ext == 'net' || $ext == 'org' || $ext == 'edu' || $ext == 'mil' || $ext == 'gov' || $ext == 'dsn') {
+	if (!empty($price[$ext])) {
 		if ($price[$ext] > $user['cash']) {
 			die_error('Insufficient balance. Try again when you have more money.', 402);
 		} else {
@@ -110,45 +111,41 @@ if (sizeof($domain) == 4 && validIP($d)) {
 	$name = array_pop($domain);
 	$host = implode('.', $domain);
 	$price = 20;
-	if ($ext == 'com' || $ext == 'net' || $ext == 'org' || $ext == 'edu' || $ext == 'mil' || $ext == 'gov' || $ext == 'dsn' || ($ext == 'usr' && $user['username'] == $domain[1])) {
-		$dInfoRoot = getDomainInfo($name . '.' . $ext);
-		if ($dInfoRoot[1] != $user['id']) {
-			die_error($dInfoRoot[1] . '  ' . $user['id'] . '  You must be the owner of the full domain to register a sub domain.', 403);
-		} else if ($price > $user['cash']) {
-			die_error('Insufficient balance. Try again when you have more money.', 402);
-		} else {
-			if (transaction($uid, BANK_USER_ID, 'Domain Registration: ' . $d, $price)) {
-				// Generate IP
-				$randomip;
-				if (!empty($fixedip)) {
-					$randomip = $fixedip;
-				} else {
-					$res;
-					$stmt = $db->prepare("SELECT * FROM iptable WHERE ip=?");
-					do {
-						$randomip = rand(1, 254) . "." . rand(0, 255) . "." . rand(0, 255) . "." . rand(0, 255);
-						$stmt->bind_param('s', $randomip);
-						$stmt->execute();
-						$res = $stmt->get_result();
-					} while ($res->num_rows != 0);
-				}
-
-				$keycode = make_keycode();
-				$stmt = $db->prepare("INSERT INTO iptable (owner, ip, regtype, time, keycode) VALUES (?, ?, 'SUBDOMAIN', ?, ?)");
-				$stmt->bind_param('isis', $uid, $randomip, $timestamp, $keycode);
-				$stmt->execute();
-				$id = $db->insert_id;
-				$stmt = $db->prepare("INSERT INTO subdomain (id, hostid, name) VALUES (?, ?, ?)");
-				$stmt->bind_param('iss', $id, $dInfoRoot[0], $host);
-				$stmt->execute();
-
-				die('Registration complete for ' . $d . ', you have been charged $' . $price);
-			} else {
-				die_error('Registration of ' . $d . ' has been DECLINED by the Dark Signs Bank.newlineCheck your bank account for further details.', 402);
-			}
-		}
+	$dInfoRoot = getDomainInfo($name . '.' . $ext);
+	if ($dInfoRoot[1] != $user['id']) {
+		die_error($dInfoRoot[1] . '  ' . $user['id'] . '  You must be the owner of the full domain to register a sub domain.', 403);
+	} else if ($price > $user['cash']) {
+		die_error('Insufficient balance. Try again when you have more money.', 402);
 	} else {
-		die_error('Invalid domain extension.');
+		if (transaction($uid, BANK_USER_ID, 'Domain Registration: ' . $d, $price)) {
+			// Generate IP
+			$randomip;
+			if (!empty($fixedip)) {
+				$randomip = $fixedip;
+			} else {
+				$res;
+				$stmt = $db->prepare("SELECT * FROM iptable WHERE ip=?");
+				do {
+					$randomip = rand(1, 254) . "." . rand(0, 255) . "." . rand(0, 255) . "." . rand(0, 255);
+					$stmt->bind_param('s', $randomip);
+					$stmt->execute();
+					$res = $stmt->get_result();
+				} while ($res->num_rows != 0);
+			}
+
+			$keycode = make_keycode();
+			$stmt = $db->prepare("INSERT INTO iptable (owner, ip, regtype, time, keycode) VALUES (?, ?, 'SUBDOMAIN', ?, ?)");
+			$stmt->bind_param('isis', $uid, $randomip, $timestamp, $keycode);
+			$stmt->execute();
+			$id = $db->insert_id;
+			$stmt = $db->prepare("INSERT INTO subdomain (id, hostid, name) VALUES (?, ?, ?)");
+			$stmt->bind_param('iss', $id, $dInfoRoot[0], $host);
+			$stmt->execute();
+
+			die('Registration complete for ' . $d . ', you have been charged $' . $price);
+		} else {
+			die_error('Registration of ' . $d . ' has been DECLINED by the Dark Signs Bank.newlineCheck your bank account for further details.', 402);
+		}
 	}
 }
 
