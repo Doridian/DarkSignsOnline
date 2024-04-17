a0a2e03af83a26a60ebcac15c66d8b1cae4a3728
Doridian <git@doridian.net>
2024-04-16 23:44:23 -0700

    try using transactions

--- server/api/transfer.php
+++ server/api/transfer.php
@@ -10,8 +10,9 @@ $amount = (int)$_REQUEST['amount'];
 
 $to = userToId($_REQUEST['to']);
 $description = $_REQUEST['description'];
+$db->begin_transaction();
 $status = transaction($user['id'], $to, $description, $amount);
-
+$db->commit();
 if ($ver > 1) {
 	die($status);
 }
