1a45b3360858c7fa2bb8f8ed1a7e0794f277e07b
Doridian <git@doridian.net>
2024-04-18 12:05:46 -0700

    Fix auth

--- server/api/auth.php
+++ server/api/auth.php
@@ -4,9 +4,5 @@ require_once('function.php');
 
 echo '1001';
 
-$stmt = $db->prepare("SELECT ipt.ip FROM iptable ipt, domain d WHERE d.name=? AND d.ext='usr' AND d.id=ipt.id") or die($db->error);
-$stmt->bind_param('s', $u);
-$stmt->execute();
-$res = $stmt->get_result();
-$ip = $res->fetch_row()[0];
-echo $ip;
+$d = getDomainInfo($user['username'] . '.usr');
+echo $d['ip'];
