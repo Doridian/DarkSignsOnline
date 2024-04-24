611c9ed34c9a2887e1505a32f94c344deca6e805
Doridian <git@doridian.net>
2024-04-23 21:03:56 -0700

    Add libraries.php

---
+++ server/api/libraries.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('function.php');
+
+$getlib = trim($_REQUEST['get']);
+if (!empty($getlib)) {
+    $stmt = $db->prepare('SELECT data FROM libraries WHERE hash = ?');
+    $stmt->bind_param('s', $getlib);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $lib = $result->fetch_assoc();
+    if (!$lib) {
+        die_error('not found', 404);
+    }
+    die($lib['data']);
+}
+
+$putlib = trim($_REQUEST['put']);
+if (!empty($putlib)) {
+    $data = $_REQUEST['data'];
+    $myhash = dso_hash($data);
+    if ($myhash !== $putlib) {
+        die_error('hash mismatch', 409);
+    }
+    $time = time();
+    $stmt = $db->prepare('INSERT INTO libraries (hash, data, owner, time) VALUES (?, ?, ?, ?);');
+    $stmt->bind_param('ssii', $putlib, $data, $user['id'], $time);
+    $stmt->execute();
+    die('OK');
+}
+
+die_error('bad request');
