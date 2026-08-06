7425c46438187e243996c40a9569f14ff9547a4e
Doridian <git@doridian.net>
2026-08-05 23:09:57 -0700

    Allow setting SMTP port

--- server/www/api/function_email.php
+++ server/www/api/function_email.php
@@ -1,7 +1,6 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
 require_once('phpmailer/Exception.php');
 require_once('phpmailer/PHPMailer.php');
@@ -10,12 +9,14 @@ require_once('phpmailer/SMTP.php');
 global $mailer;
 $mailer = new PHPMailer(true);
 $mailer->isSMTP(true);
-$mailer->SMTPAuth = true;
-$mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mailer->Host = $SMTP_HOST;
-$mailer->Port = 587;
-$mailer->Username = $SMTP_USERNAME;
-$mailer->Password = $SMTP_PASSWORD;
+$mailer->Port = (int)$SMTP_PORT;
+if (!empty($SMTP_USERNAME)) {
+    $mailer->SMTPAuth = true;
+    $mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mailer->Username = $SMTP_USERNAME;
+    $mailer->Password = $SMTP_PASSWORD;
+}
 
 function send_email($to_email, $to_name, $subject, $message) {
     global $mailer, $SMTP_FROM;
